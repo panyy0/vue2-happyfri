e62248b8b6d8cae5672a319c4536a8620a1eac38
pyy <panyy0@163.com>
2017-11-18 10:33:51 +0800

    banner问题 顶部搜索组件化

---
+++ src/components/TopBar.vue
@@ -0,0 +1,122 @@
+<template>
+  <!--加个样式，方便以后修改-->
+
+  <div class="top-bar mod">
+    <img class='top-bar-user' :src='iconBaby'/>
+
+    <div class='top-bar-search'>
+      <div>
+        <img :src='iconSearch'/>
+        <span class='text'>今天想听什么</span>
+      </div>
+    </div>
+    <!--<div class='top-bar-search'>-->
+      <!--<div class='search-icon-view'>-->
+        <!--<img :src='iconSearch'/>-->
+      <!--</div>-->
+      <!--<div class="section text-view">-->
+        <!--<div class='text'>今天想听什么</div>-->
+      <!--</div>-->
+    <!--</div>-->
+
+
+    <img class="top-bar-news" :src='iconNews'/>
+  </div>
+
+
+
+
+</template>
+
+<script>
+
+
+  import ICON_BABY from '../images/tab/home_pic_baby@2x.png';
+  import ICON_SEARCH from '../images/tab/home_icon_search@2x.png';
+  import ICON_NEWS from '../images/tab/home_icon_news@2x.png';
+
+  export default {
+    name: 'SysTopBar'
+    ,data () {
+      return {
+        iconBaby: ICON_BABY
+        ,iconSearch: ICON_SEARCH
+        ,iconNews: ICON_NEWS
+      }
+    }
+    ,computed: {
+    }
+    ,components: {},
+    created(){
+    },
+    mounted: function() {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .top-bar {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    position: absolute;
+    z-index: 500;
+    top: 0;
+    width: 100%;
+    height: 0.6rem;
+    /*background-color: #aaa;*/
+
+    .top-bar-user {
+      position: absolute;
+      left: 0;
+      margin-left: 0.22rem;
+      width: 0.6rem;
+      height: 0.6rem;
+    }
+
+    .top-bar-news {
+      position: absolute;
+      right: 0;
+      margin: 0.06rem 0.2rem 0.06rem 0;
+      width: 0.48rem;
+      height: 0.48rem;
+    }
+
+    .top-bar-search {
+      position: absolute;
+      left: 1.3rem;
+      top: 0.02rem;
+      width: 5rem;
+      height: 0.56rem;
+      border-radius: 1rem;
+      background: rgba(255, 255, 255, .3);
+
+      img {
+        position: absolute;
+        width: 0.3rem;
+        height: 0.3rem;
+        top: 0.13rem;
+        left: 0.3rem;
+      }
+
+      .text {
+        position: absolute;
+        left: 0.85rem;
+        top: 0.12rem;
+        font-size: 0.26rem;
+        color: #fff;
+      }
+    }
+
+  }
+
+
+
+  /*底部盒子固定，元素浮动，盒子随着元素个数增加而没有撑大*/
+  /*解决方法：使用clear 或者 overflow：hidden;*/
+</style>
--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -3,28 +3,7 @@
       <div class="home_container"  id="homeContainer">
         <div>
           <div>
-            <div class="top-bar">
-              <div class="section">
-                <img class='image' src='../../images/tab/home_pic_baby@2x.png'/>
-              </div>
-
-              <div class='operate'>
-                <div class='search-icon-view'>
-                  <img src='../../images/tab/home_icon_search@2x.png'/>
-                </div>
-                <div class="section text-view">
-                  <div class='text'>今天想听什么</div>
-                </div>
-                <div class='scan-icon-view'>
-                  <img src='../../images/tab/home_icon_scan@2x.png'/>
-                </div>
-              </div>
-
-              <div class='news-icon-view'>
-                <img src='../../images/tab/home_icon_news@2x.png'/>
-              </div>
-
-            </div>
+            <SysTopBar></SysTopBar>
 
             <swiper auto  height="4.4rem" dots-position="center" dots-class="custom-bottom" v-model="bannerIndex"
                     @on-index-change="onBannerIndexChange">
@@ -81,6 +60,7 @@
 <script>
   import {Swiper, SwiperItem} from 'vux';
   import SysTabBar from '../../components/TabBar'
+  import SysTopBar from '../../components/TopBar'
   import IScroll from "iscroll";
 
   import BANNER_1 from '../../images/banner/banner@2x.png';
@@ -168,6 +148,7 @@
     },
     components: {
       SysTabBar
+      , SysTopBar
       , Swiper
       , SwiperItem
     },
@@ -219,82 +200,8 @@
     bottom: 1.1rem;
     top:0;
 
-    .top-bar {
-      float: left;
-      position: absolute;
-      z-index: 999;
-      margin: 0.56rem 0 0 0.2rem;
-      height: 0.6rem;
-
-      .section {
-        float: left;
-        position: relative;
-        height: 100%;
-
-        .image {
-          float: left;
-          width: 0.6rem;
-          height: 100%;
-          border-radius: 0.6rem;
-        }
-
-        .text {
-          height: 100%;
-          width: 100%;
-          font-size: 0.26rem;
-          padding: 0.11rem 0;
-          color: #fff;
-        }
-      }
-
-      .operate {
-        float: left;
-        margin: 0.01rem 0.42rem 0.01rem 0.38rem;
-        border-radius: 1rem;
-        width: 5.2rem;
-        height: 0.54rem;
-        background: rgba(255, 255, 255, .3);
-
-        .search-icon-view {
-          float: left;
-          height: 100%;
-
-          img {
-            float: left;
-            margin: 0.12rem 0.25rem 0 0.3rem;
-            width: 0.3rem;
-            height: 0.3rem;
-          }
-        }
-
-        .scan-icon-view {
-          float: right;
-          height: 100%;
-
-          img {
-            float: right;
-            margin: 0.12rem 0.29rem;
-            width: 0.3rem;
-            height: 0.3rem;
-          }
-        }
-
-        .text-view {
-          width: 66%;
-        }
-      }
-
-      .news-icon-view {
-        float: left;
-        height: 100%;
-        margin: 0.06rem 0.2rem 0 0;
-
-        img {
-          float: right;
-          width: 0.48rem;
-          height: 0.48rem;
-        }
-      }
+    .mod {
+      top: 0.2rem;
     }
 
     .items {
