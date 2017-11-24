679daf63515d1621979c06fdc9067eac3084a390
pyy <panyy0@163.com>
2017-11-24 15:12:39 +0800

    我的主页

--- src/page/mine/index.vue
+++ src/page/mine/index.vue
@@ -3,7 +3,18 @@
     <div class="container">
       <div class="info">
         <div class="info-top">
+          <div class="top">
+            <span>{{ info.title }}</span>
+            <img :src="info.icon" />
+          </div>
+          <div class="center">
+            <img :src="info.avatar" />
 
+          </div>
+          <div class="bottom">
+            <span class="name">{{ info.name }}</span>
+            <span class="desc">{{ info.desc }}</span>
+          </div>
         </div>
         <div class="info-bottom">
           <div class="item" v-for="item in funcList">
@@ -31,6 +42,8 @@
   import MTabBar from 'components/MTabBar'
   import MGroup from 'components/MGroup'
   import {Group, Cell} from 'vux';
+  import PIC_HEAD from 'images/mine/pic_head@2x.png';
+  import ICON_SET from 'images/mine/icon_set@2x.png';
   import ICON_ARROW from 'images/mine/icon_arrow@2x.png';
   import ICON_DOWNLOAD from 'images/mine/icon_download@2x.png';
   import ICON_ANCHOR from 'images/mine/icon_anchor@2x.png';
@@ -50,7 +63,14 @@
     , data() {
         return {
 
-          funcList: [
+          info: {
+              title: '管理宝宝'
+              , name: '贝贝妈'
+              , desc: '你是有灵性的天使'
+              , avatar: PIC_HEAD
+              , icon: ICON_SET
+          }
+          ,funcList: [
             {
                 'title': '最近播放'
                 , 'img': ICON_HISTORY
@@ -143,6 +163,63 @@
           height: 4.94rem;
           background: #fff url("@{images}/bg_banner@2x.png") no-repeat;
           background-size:100% 100%;-moz-background-size:100% 100%;
+
+          .top {
+            position: absolute;
+            margin-top: 0.3rem;
+            overflow: hidden;
+            width: 100%;
+            span {
+              float: left;
+              margin-left: 0.22rem;
+              font-size: 0.28rem;
+              color: #fff;
+            }
+
+            img {
+              float: right;
+              width: 0.44rem;
+              height: 0.44rem;
+              margin-right: 0.2rem;
+            }
+          }
+
+          .center {
+            position: relative;
+            display: block;
+            overflow: hidden;
+            width: 100%;
+            top: 1.46rem;
+
+            img {
+              margin: 0 auto;
+              width: 1.6rem;
+              height: 1.6rem;
+              border-radius: 1rem;
+              display: inherit;
+            }
+          }
+
+          .bottom {
+            position: relative;
+            top: 1.45rem;
+
+            .name {
+              margin-top: 0.2rem;
+              display: block;
+              text-align: center;
+              color: #fff;
+              font-size: 0.34rem;
+            }
+
+            .desc {
+              margin-top: 0.26rem;
+              display: block;
+              text-align: center;
+              color: #fff;
+              font-size: 0.26rem;
+            }
+          }
         }
         .info-bottom {
           background-color: #fff;
