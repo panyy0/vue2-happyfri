99030c6384e210a0d97f53406f588ee49df6a2cc
pyy <panyy0@163.com>
2017-11-26 17:55:01 +0800

    分类页面

--- src/page/classify/index.vue
+++ src/page/classify/index.vue
@@ -1,6 +1,24 @@
 <template>
   	<div>
     	<MHeader :title="headerData.title" :iconRight="headerData.iconRight" :contentRight="headerData.contentRight"></MHeader>
+      <div id="wrapper">
+        <div class="container">
+          <div style="padding: 0.4rem 0.22rem; display: flex; flex-direction: column; background-color: #fff; border: 1px solid;">
+            <div style="display: flex; overflow: hidden; border: 1px solid;">
+              <span style="font-size: 0.34rem; color: #222;">热门分类</span>
+            </div>
+            <div style="margin-top: 0.4rem; border-radius: 0.1rem; border: 1px solid #ff7391;">
+              <div style="width: 1.38rem; height: 1.4rem; background-color: #ff7391;">
+                <div style="height: 100%; width: 0.60rem; display: flex; flex-direction: column; position:relative; margin-left: 0.4rem; border: 1px solid;">
+                  <img style="width: 0.5rem; height: 0.5rem;" :src="iconFunction" />
+                  <span style="font-size: 0.28rem; color: #fff;">功能</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div style="margin: 0.16rem auto; background-color: #fff; border: 1px solid;"></div>
+        </div>
+      </div>
     	<MTabBar></MTabBar>
   	</div>
 </template>
@@ -10,6 +28,7 @@ import MTabBar from 'components/MTabBar';
 import MHeader from 'components/MHeader';
 
 import ICON_RIGHT from 'images/classify/icon_screen@2x.png'
+import ICON_FUNCTION from 'images/classify/icon_function@2x.png'
 
 
 export default {
@@ -22,6 +41,7 @@ export default {
           , 'iconRight': ICON_RIGHT
           , 'contentRight': '年龄筛选'
         }
+        , iconFunction: ICON_FUNCTION
       }
     }
   	,components: {
@@ -39,5 +59,15 @@ export default {
 </script>
 
 <style lang="less" scoped="page">
-
+  #wrapper {
+    position: absolute;
+    top: 0.88rem;
+    left: 0;
+    width: 100%;
+    bottom: 1.4rem;
+    overflow-y: auto;
+    .container {
+      background-color: #f2f2f2;
+    }
+  }
 </style>
